fa2feb198f594284b6dcd307b9f847037ed5dd07
gexen2 <inghedenadam@gmail.com>
2019-12-10 12:08:27 +0100

    Changed: Button color change

--- update.sh
+++ update.sh
@@ -1,4 +1,5 @@
 rm -rf /Applications/MAMP/htdocs/magento2/pub/static/*
 rm -rf /Applications/MAMP/htdocs/magento2/var/view_preprocessed/*
+php /Applications/MAMP/htdocs/magento2/bin/magento setup:static-content:deploy sv_SE en_US -f
 php /Applications/MAMP/htdocs/magento2/bin/magento cache:clean
 php /Applications/MAMP/htdocs/magento2/bin/magento cache:flush
